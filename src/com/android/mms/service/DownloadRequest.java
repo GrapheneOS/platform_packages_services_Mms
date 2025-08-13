30f74bf269144e3def94b2a278963ac364c75837
Matthew Yu <matthewmingyu@google.com>
2025-08-13 15:41:49 -0700

    Verify calling user on mms downloads
    Bug: 305710989
    Test: manually tested sending/downloading MMS with the primary user. I created a secondary user and also verified that sending/downloading MMS's work on the secondary user's account
    Flag: EXEMPT bugfix
    
    (cherry picked from commit 0b8c94aa0a37c83d03f6c442c8d111fe8f49bbac)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:efda719b961b01ef63054670ec4ab308dae15bdf
    Merged-In: I01782ba1358b64d49ccbab0a106b60deaa9262fa
    Change-Id: I01782ba1358b64d49ccbab0a106b60deaa9262fa

--- src/com/android/mms/service/DownloadRequest.java
+++ src/com/android/mms/service/DownloadRequest.java
@@ -59,6 +59,7 @@ public class DownloadRequest extends MmsRequest {
     private final String mLocationUrl;
     private final PendingIntent mDownloadedIntent;
     private final Uri mContentUri;
+    private final int mCallingUser;
 
     public DownloadRequest(RequestManager manager, int subId, String locationUrl,
             Uri contentUri, PendingIntent downloadedIntent, int callingUser, String creator,
@@ -69,6 +70,7 @@ public class DownloadRequest extends MmsRequest {
         mLocationUrl = locationUrl;
         mDownloadedIntent = downloadedIntent;
         mContentUri = contentUri;
+        mCallingUser = callingUser;
     }
 
     @Override
@@ -255,7 +257,7 @@ public class DownloadRequest extends MmsRequest {
      */
     @Override
     protected boolean transferResponse(Intent fillIn, final byte[] response) {
-        return mRequestManager.writePduToContentUri(mContentUri, response);
+        return mRequestManager.writePduToContentUri(mContentUri, response, mCallingUser);
     }
 
     @Override
